0ac7c45b75c434486b91e3354d8843166635a3d1
Suman Kumar <sumancsnit@gmail.com>
2020-05-23 22:46:11 +0530

    contact component added

---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = ({ contact }) => {
+  const { name, phone, email } = contact;
+  return (
+    <tr>
+      <td>
+        <div className='custom-control custom-checkbox'>
+          <input type='checkbox' className='custom-control-input' />
+          <label className='custom-control-label'></label>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>{phone}</td>
+      <td>{email}</td>
+    </tr>
+  );
+};
+
+export default Contact;
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,36 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Contact from './Contact';
 
 export default function Contacts() {
+  const contacts = useSelector((state) => state.contacts);
   return (
     <div>
-      <table class='table table-dark'>
-        <thead>
+      <table className='table shadow'>
+        <thead className='bg-danger text-white'>
           <tr>
-            <th scope='col'>#</th>
-            <th scope='col'>First</th>
-            <th scope='col'>Last</th>
-            <th scope='col'>Handle</th>
+            <th scope='col'>
+              <div className='custom-control custom-checkbox'>
+                <input type='checkbox' className='custom-control-input' />
+                <label className='custom-control-label'></label>
+              </div>
+            </th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>E-mail</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope='row'>1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope='row'>2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope='row'>3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {contacts.map((contact, idx) => (
+            <Contact key={`${contact.name}idx`} contact={contact} />
+          ))}
         </tbody>
       </table>
     </div>
